e1e216551b89bb3ba99a1b039bb0710f3d3e76c0
Logan Manuel <itsloganmanuel@gmail.com>
2016-05-11 09:04:55 -0400

    Add files via upload

---
+++ build/javascripts/all.js
@@ -0,0 +1,19 @@
+particlesJS.load('particles-js', '../particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+var menuButton = document.getElementById('menuButton');
+var sideMenu = document.getElementById('sideMenu');
+menuButton.addEventListener('click', function (e) {
+    menuButton.classList.toggle('is-active');
+    sideMenu.classList.toggle('is-active');
+    e.preventDefault();
+});
+
+var socialButton = document.getElementById('socialButton');
+var socialMenu = document.getElementById('socialMenu');
+socialButton.addEventListener('click', function(e) {
+  socialButton.classList.toggle('is-active');
+  socialMenu.classList.toggle('is-active');
+  e.preventDefault();
+});
